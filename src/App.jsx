6fc8a3294de1eba992b8b604721d216e75f4828d
wdxGit <1091830282@qq.com>
2020-03-19 08:48:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Hello !!</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
